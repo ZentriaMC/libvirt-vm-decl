9d26cd3639c1680bb85a06944c36a8020d3c4e96
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-03-15 19:56:33 +0200

    Generate multiple machines, add units helper

--- gen.nix
+++ gen.nix
@@ -2,13 +2,21 @@ let
   flake = builtins.getFlake (toString ./.);
   system = builtins.currentSystem;
   pkgs = flake.inputs.nixpkgs.legacyPackages.${system};
-
   decl = pkgs.callPackage ./vm-xml.nix { };
-in
 
-decl {
-  name = "github-worker-0";
-  memory = 1073740000;
-  nic = "virtbr0";
-  mac = "1e:08:53:d2:29:cd";
-}
+  inherit (import ./units.nix) KiB MiB GiB TiB;
+in
+map decl [
+  {
+    name = "github-worker-0";
+    memory = GiB 1; # 1073740000;
+    nic = "virtbr0";
+    mac = "1e:08:53:d2:29:cd";
+  }
+  {
+    name = "github-worker-1";
+    memory = GiB 2;
+    nic = "virtbr0";
+    mac = "1e:08:53:d2:29:ce";
+  }
+]
---
+++ units.nix
@@ -0,0 +1,6 @@
+rec {
+  KiB = k: 1024 * k;
+  MiB = m: 1024 * (KiB m);
+  GiB = g: 1024 * (MiB g);
+  TiB = t: 1024 * (GiB t);
+}
